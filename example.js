97960b4fb94564a564d0d81bc29563092c9e1099
Sumiyaakhi <sumiya.akhi793@gmail.com>
2023-01-30 12:36:29 +0600

    function add

---
+++ example.js
@@ -0,0 +1,12 @@
+function getAverage (assignment1, assignment2, assignment3){
+    const total = assignment1+ assignment2 + assignment3;
+    const average = total / 3;
+    return average ;
+}
+
+const assignment1Mark = 60;
+const assignment2Mark = 58;
+const assignment3Mark = 60;
+
+var myAverage = getAverage(assignment1Mark, assignment2Mark,assignment3Mark);
+console.log('my average so far: ' , myAverage);
